3efe050dbaa109b494c29ee2d7252b1013fc2a06
Artem V. Navrotskiy <bozaro@yandex.ru>
2016-11-22 15:04:54 +0300

    Add sample for database interaction

--- main.go
+++ main.go
@@ -1,18 +1,49 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"fmt"
+	"github.com/golang/glog"
 	"net/http"
+	"os"
 
 	"gopkg.in/macaron.v1"
+
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: example -stderrthreshold=[INFO|WARN|FATAL] -log_dir=[string]\n")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func init() {
+	flag.Usage = usage
+	// NOTE: This next line is key you have to call flag.Parse() for the command line
+	// options or "flags" that are defined in the glog module to be picked up.
+	flag.Parse()
+}
+
 func main() {
+	db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/hello")
+	if err != nil {
+		glog.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		glog.Warning(err)
+	}
+
+	defer db.Close()
+
 	m := macaron.Classic()
 	m.Get("/", myHandler)
 
-	log.Println("Server is running...")
-	log.Println(http.ListenAndServe("0.0.0.0:4000", m))
+	glog.Info("Server is running...")
+	glog.Info(http.ListenAndServe("0.0.0.0:4000", m))
 }
 
 func myHandler(ctx *macaron.Context) string {
