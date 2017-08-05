301f2288eb57d1d138dec33d91137184cb79d466
Artem V. Navrotskiy <bozaro@yandex.ru>
2017-08-05 23:45:53 +0300

    Base iron application

---
+++ src/endpoints.rs
@@ -0,0 +1,6 @@
+use context::Context;
+use mount::Mount;
+
+pub fn declare_endpoints(ctx: Context) -> Mount {
+    Mount::new()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+extern crate dotenv;
+#[macro_use]
+extern crate iron;
+extern crate hyper;
+extern crate router;
+extern crate mount;
+
+mod context;
+mod endpoints;
+
+use dotenv::dotenv;
+use context::Context;
+use endpoints::declare_endpoints;
+use hyper::net::{HttpStream, NetworkListener};
+
+use iron::Protocol;
+use iron::prelude::Iron;
+use std::env;
+use std::io;
+use std::net::SocketAddr;
+use std::net::TcpListener;
+use std::sync::Arc;
+
+#[derive(Clone)]
+struct TcpListenerNoDelay {
+    listener: Arc<TcpListener>,
+}
+
+impl NetworkListener for TcpListenerNoDelay {
+    type Stream = HttpStream;
+
+    fn accept(&mut self) -> Result<Self::Stream, hyper::Error> {
+        let tcp = try!(self.listener.accept());
+        try!(tcp.0.set_nodelay(true));
+        let stream = HttpStream(tcp.0);
+        Ok(stream)
+    }
+
+    fn local_addr(&mut self) -> io::Result<SocketAddr> {
+        self.listener.local_addr()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let router = declare_endpoints(Context {});
+
+    // Start server
+    let addr = env::var("LISTEN").unwrap_or("localhost:5000".to_string());
+    println!("http://{}/", addr);
+
+    let listener = TcpListener::bind(addr).unwrap();
+    Iron::new(router)
+        .listen(
+            TcpListenerNoDelay { listener: Arc::new(listener) },
+            Protocol::http(),
+        )
+        .unwrap();
 }
